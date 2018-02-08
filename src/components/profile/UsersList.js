3ff1e7d57d1d5425fc3ad331aa17053881428b45
buckethead1986 <rspeckpfs@gmail.com>
2018-02-07 23:36:49 -0500

    following userList updates correctly

--- src/components/profile/UsersList.js
+++ src/components/profile/UsersList.js
@@ -17,15 +17,7 @@ const styles = {
   overflow: "auto"
 };
 
-//splits the list of users into followed and unfollowed categories minus the current user, and creates list it
 class UsersList extends React.Component {
-  // constructor() {
-  //   super();
-  //
-  //   this.state = {
-  //     users: []
-  //   };
-  // }
   componentDidMount() {
     console.log("componentDidMount", this.props.relationships);
     this.splitUsers(
@@ -33,84 +25,58 @@ class UsersList extends React.Component {
       this.props.relationships
     );
   }
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log("should component update");
-  //   console.log(this.props.currUser[0].id, nextProps.currUser[0].id);
-  //   return this.props.relationships === nextProps.relationships;
-  // }
 
-  // componentWillReceiveProps(nextProps) {
-  //   console.log(
-  //     "componentWillReceiveProps",
-  //     this.props.relationships,
-  //     nextProps.relationships
-  //   );
-  //   if (this.props.relationships !== nextProps.relationships) {
-  //     // this.setState({
-  //     //
-  //     // })
-  //     this.splitUsers(
-  //       this.filterForCurrentUser(nextProps.users),
-  //       nextProps.relationships
-  //     );
-  //   }
-  // }
-
+  //removes the current user from the list of users
   filterForCurrentUser = userList => {
     return userList.filter(user => {
       return user.id !== this.props.currUser[0].id;
     });
   };
 
+  //iterates through all relationships, when one has a follower_id (doing the following) equal to the current users id,
+  //the user with the followed_id (being followed) of that relationship is pushed onto the followed array, and is removed
+  //from the usersClone array.
   splitUsers = (users, relationships) => {
+    let usersClone = users.slice(0);
+    // let filteredUsers = [];
     followed = [];
     unFollowed = [];
-    users.filter(user => {
-      return user.id === this.props.currUser[0].id;
-    });
 
-    // for (var arr in users) {
-    //   console.log(users[arr].id, this.props.currUser[0].id);
-    //   for (var filter in following) {
-    //     console.log(users);
-    //     console.log(users[arr], following[filter]);
-    //     if (users[arr].id === following[filter].id) {
-    //       this.remove(users, users[arr]);
-    //     }
-    //   }
-    // }
-    console.log(users, relationships, this.props.currUser[0].following);
-    // let x = [];
     for (var relationship in relationships) {
-      console.log(relationships[relationship]);
       if (
         relationships[relationship].follower_id === this.props.currUser[0].id
       ) {
-        followed.push(
-          users.find(user => {
-            console.log(user, relationships[relationship]);
-            return user.id === relationships[relationship].followed_id;
-          })
-        );
+        const user = users.find(user => {
+          return user.id === relationships[relationship].followed_id;
+        });
+        followed.push(user);
+        // debugger;
+        // filteredUsers = usersClone.filter(user => {
+        //   return user.id !== relationships[relationship].followed_id;
+        // });
+        // usersClone = filteredUsers;
+        this.remove(usersClone, user);
       }
     }
-    console.log(followed);
-    // followed = this.props.currUser[0].following;
-    // unFollowed = users;
-
+    //if nothing happened, defaults to the full users list
+    // if (usersClone.length === users.length) {
+    //   filteredUsers = users;
+    // }
+    console.log(usersClone);
+    // debugger;
+    unFollowed = usersClone;
+    //I don't like calling forceUpdate, but I needed this component to rerender based on following/unfollowing users in another component.
     this.forceUpdate();
   };
 
   remove = (array, element) => {
     const index = array.indexOf(element);
-
     if (index !== -1) {
       array.splice(index, 1);
     }
   };
 
   render() {
-    console.log(this.props.relationships);
     const followedUsersList = followed.map(user => {
       return (
         <ListItem
