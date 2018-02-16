e87ef275f63e2cdd1afc191ed72f2939527b106d
buckethead1986 <rspeckpfs@gmail.com>
2018-02-16 12:52:08 -0500

    fixing ajax issue with fetchCurrentUser

--- src/App.js
+++ src/App.js
@@ -118,6 +118,7 @@ class App extends Component {
             })
           },
           () => {
+            console.log(this.state.currUser);
             if (this.state.currUser !== []) {
               this.props.store.dispatch({
                 type: "CHANGE_IMAGE",
